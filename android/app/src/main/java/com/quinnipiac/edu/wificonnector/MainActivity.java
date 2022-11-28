24bbabd208036e2f2f81f8dd35640413868c7584
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-28 14:07:58 -0500

    Added device list ui

--- android/app/src/main/java/com/quinnipiac/edu/wificonnector/MainActivity.java
+++ android/app/src/main/java/com/quinnipiac/edu/wificonnector/MainActivity.java
@@ -2,7 +2,14 @@ package com.quinnipiac.edu.wificonnector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.harrysoft.androidbluetoothserial.BluetoothManager;
+
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothManager bluetoothManager = BluetoothManager.getInstance();
+        if(bluetoothManager == null) {
+            Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        ListView deviceList = findViewById(R.id.list_devices);
+
+
+    }
+
+    void refreshDevices() {
+        Collection<BluetoothDevice> pairedDevices = BluetoothManager.getInstance().getPairedDevicesList();
+
+
     }
 }
