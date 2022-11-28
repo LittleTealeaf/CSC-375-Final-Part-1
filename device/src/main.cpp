9c6b8a251ce6c9b1549b62a1319ce14e87a19d5c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-28 17:29:05 -0500

    Set up device bluetooth

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 
-
+BluetoothSerial Bluetooth;
 
 void setup(){
 	Serial.begin(115200);
+	Bluetooth.begin("LittleTealeaf/CSC-375-Final");
 }
 
 void loop(){
