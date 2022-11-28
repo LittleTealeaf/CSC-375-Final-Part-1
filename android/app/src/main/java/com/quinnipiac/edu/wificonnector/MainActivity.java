4ede4bf9764d4fb02822269f0a813b2b50c693f6
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-28 13:34:36 -0500

    Initialized Android Application Project

---
+++ android/app/src/main/java/com/quinnipiac/edu/wificonnector/MainActivity.java
@@ -0,0 +1,14 @@
+package com.quinnipiac.edu.wificonnector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
