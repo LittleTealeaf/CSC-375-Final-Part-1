32dc4f2ddaa9d192c8fcdc76b435a3e9d10e96d1
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-11-28 15:32:35 -0500

    Working on getting bluetooth to work

--- android/app/src/main/java/com/quinnipiac/edu/wificonnector/MainActivity.java
+++ android/app/src/main/java/com/quinnipiac/edu/wificonnector/MainActivity.java
@@ -1,23 +1,45 @@
 package com.quinnipiac.edu.wificonnector;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-import com.harrysoft.androidbluetoothserial.BluetoothManager;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MAIN_ACTIVITY";
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(), this::onBluetoothPermissionGranted);
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothManager bluetoothManager = BluetoothManager.getInstance();
-        if(bluetoothManager == null) {
-            Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_LONG).show();
-            finish();
+        textView = findViewById(R.id.text_debug);
+
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if(!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            requestPermissionLauncher.launch(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        } else {
+            Log.d(TAG, "onCreate: Bluetooth is already enabled");
         }
 
     }
 
+    public void onBluetoothPermissionGranted(boolean isGranted) {
+
+    }
 }
