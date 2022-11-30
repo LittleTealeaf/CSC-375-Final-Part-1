1e6a587a17558e72e5be36b81a841d7df62a42c9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-30 10:20:00 -0500

    created convertStringtoMessage function

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -6,9 +6,37 @@
 BluetoothSerial Bluetooth;
 
 struct Message {
-	char *command;
-	char *content;
+	char *type;
+	char *message;
 };
+
+Message convertStringtoMessage(String string) {
+	Message message;
+
+	int lengthType = 0;
+	int lengthMessage = 0;
+	char *str = string.begin();
+
+	for(int i = 0; i < string.length(); i++) {
+		if(str[i] == '|') {
+			lengthType = i;
+			lengthMessage = string.length() - i;
+		}
+	}
+
+	message.type = (char*) malloc(sizeof(char) * lengthType);
+	message.message = (char*) malloc(sizeof(char) * lengthMessage);
+
+	for(int i = 0; i < string.length(); i++) {
+		if(i < lengthType) {
+			message.type[i] = str[i];
+		} else if(i > lengthType) {
+			message.message[i] = str[i];
+		}
+	}
+
+	return message;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -18,5 +46,8 @@ void setup() {
 void loop() {
   if (Bluetooth.available()) {
     String message = Bluetooth.readStringUntil('\n');
+
+		//"MessageType|MessageContent>"
+
   }
 }
