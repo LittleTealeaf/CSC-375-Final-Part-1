3359938595ceb823873d85cb71001b66923a658d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-30 13:32:25 -0500

    Reset main.cpp

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -5,62 +5,6 @@
 
 BluetoothSerial Bluetooth;
 
-#define CONNECTION Serial
-
-typedef enum Topic {
-	PACKET_QUERY_WIFI,
-	PACKET_CONNECT,
-	PACKET_STATUS_WIFI,
-	PACKET_ERROR
-} Topic;
-
-/*
- * Packet Structure
- *
- * <Type><Content>
- *
- */
-
-struct Packet {
-  int type;
-  String content;
-};
-
-void sendPacket(Topic type, String content) {
-	CONNECTION.printf("%d%s", type, content.begin());
-}
-
-// Handles a CONNECT_WIFI packet
-void handleConnect(Packet packet) {
-	int index = packet.content.indexOf('\t');
-	String uuid = packet.content.substring(0,index);
-	String password = packet.content.substring(index);
-
-	WiFi.begin(uuid.begin(),password.begin());
-}
-
-// Handles a QUERY_WIFI_STATUS packet
-void handleQuery(Packet packet) {
-
-}
-
-void handlePacket(Packet packet) {
-  if (packet.type == PACKET_QUERY_WIFI) {
-    return handleQuery(packet);
-  }
-  if (packet.type == PACKET_CONNECT) {
-    return handleConnect(packet);
-  }
-}
-
-Packet createPacket(String message) {
-  Packet packet;
-  packet.type = message.charAt(0);
-  packet.content = message.substring(1);
-
-  return packet;
-}
-
 void setup() {
   Serial.begin(115200);
   Bluetooth.begin("LittleTealeaf/CSC-375-Final");
@@ -73,9 +17,4 @@ void setup() {
 }
 
 void loop() {
-  if (CONNECTION.available()) {
-    String message = CONNECTION.readStringUntil('\n');
-
-    Packet packet = createPacket(message);
-  }
 }
