751c8689f985c467a94186dab5219d7ae053909a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-30 10:56:42 -0500

    Fixed convertStringtoMessage Changed the type and message varaibles to String in the struct

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -6,48 +6,31 @@
 BluetoothSerial Bluetooth;
 
 struct Message {
-	char *type;
-	char *message;
+	String type;
+	String message;
 };
 
 Message convertStringtoMessage(String string) {
-	Message message;
+  Message message;
 
-	int lengthType = 0;
-	int lengthMessage = 0;
-	char *str = string.begin();
+  int index = string.indexOf("|");
 
-	for(int i = 0; i < string.length(); i++) {
-		if(str[i] == '|') {
-			lengthType = i;
-			lengthMessage = string.length() - i;
-		}
-	}
+	message.type = string.substring(0,index);
+	message.message = string.substring(index + 1);
 
-	message.type = (char*) malloc(sizeof(char) * lengthType);
-	message.message = (char*) malloc(sizeof(char) * lengthMessage);
 
-	for(int i = 0; i < string.length(); i++) {
-		if(i < lengthType) {
-			message.type[i] = str[i];
-		} else if(i > lengthType) {
-			message.message[i] = str[i];
-		}
-	}
-
-	return message;
+  return message;
 }
 
 void setup() {
   Serial.begin(115200);
   Bluetooth.begin("LittleTealeaf/CSC-375-Final");
+
+	// Why is this not working?
+  while (!Serial)
+    ;
+
 }
 
 void loop() {
-  if (Bluetooth.available()) {
-    String message = Bluetooth.readStringUntil('\n');
-
-		//"MessageType|MessageContent>"
-
-  }
 }
