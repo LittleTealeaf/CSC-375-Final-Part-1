a288a505e2016552eb66bbdc78037bf844c55992
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-30 13:31:22 -0500

    Cleaned up MainActivity

--- android/app/src/main/java/com/quinnipiac/edu/wificonnector/MainActivity.java
+++ android/app/src/main/java/com/quinnipiac/edu/wificonnector/MainActivity.java
@@ -13,33 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MAIN_ACTIVITY";
-
-    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
-            new ActivityResultContracts.RequestPermission(), this::onBluetoothPermissionGranted);
-
-    private TextView textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text_debug);
-
-        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
-        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
-
-        if(!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            requestPermissionLauncher.launch(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        } else {
-            Log.d(TAG, "onCreate: Bluetooth is already enabled");
-        }
-
-    }
-
-    public void onBluetoothPermissionGranted(boolean isGranted) {
-
+        
     }
 }
