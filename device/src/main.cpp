b59ee9fc900a4235d85a03b5be7da463db3e1b8d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-01 20:49:00 -0500

    Device now displays color on screen

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -4,14 +4,29 @@
 #include <WiFi.h>
 #include <stdlib.h>
 #include <string.h>
+#include <M5Core2.h>
 
 BluetoothSerial Bluetooth;
 
 #define DELIMINER "|"
 #define PACKET_TERMINATOR '\n'
 
+
+#define STATUS_IDLE TFT_DARKGREY
+#define STATUS_NO_SSID TFT_ORANGE
+#define STATUS_SCAN_COMPLETE TFT_OLIVE
+#define STATUS_CONNECTED TFT_GREEN
+#define STATUS_FAILED TFT_RED
+#define STATUS_LOST TFT_BLUE
+#define STATUS_DISCONNECTED TFT_NAVY
+
+
 int wifiStatus;
 Ticker wifiStatusTicker;
+
+void setScreenColor(uint32_t color) {
+	M5.Lcd.fillScreen(color);	
+}
 
 // Sends a packet, as long as the serial connection is valid
 void sendPacket(const char topic[], char *content) {
@@ -39,6 +54,21 @@ void checkWiFiStatus() {
 		}
     wifiStatus = status;
     sendWiFiStatusPacket();
+
+		if(status == 1) {
+			setScreenColor(STATUS_IDLE);
+		} else if(status == 2) {
+			setScreenColor(STATUS_NO_SSID);
+		} else if(status == 3) {
+			setScreenColor(STATUS_SCAN_COMPLETE);
+		} else if(status == 4) {
+			setScreenColor(STATUS_CONNECTED);
+		} else if(status == 5) {
+			setScreenColor(STATUS_LOST);
+		} else if(status == 6) {
+			setScreenColor(STATUS_DISCONNECTED);
+		}
+	
   }
 }
 
@@ -66,6 +96,9 @@ void handlePacket(String packet) {
 
 void setup() {
   Serial.begin(115200);
+
+	M5.begin();
+
   Bluetooth.begin("LittleTealeaf/CSC-375-Final");
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
