ead73868609d80f50f0ebff129bbca794fe43252
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-01 19:17:33 -0500

    Reduce updates to every second

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -70,7 +70,7 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
-  wifiStatusTicker.attach_ms(5000, checkWiFiStatus);
+  wifiStatusTicker.attach_ms(1000, checkWiFiStatus);
 }
 
 void loop() {
